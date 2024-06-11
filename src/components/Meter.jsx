06c90f7085ea14040d6060451c9284c3ec16bcd0
Hassaan-Ahmedd <hassutechap@gmail.com>
2024-06-12 04:37:54 +0500

    changes in ui

--- src/components/Meter.jsx
+++ src/components/Meter.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import {CircularProgress, Card, CardBody, CardFooter, Chip} from "@nextui-org/react";
 
 export default function MyMeter({title,reading,fromColor,toColor}) {
+
+
+  console.log("reading::::",reading);
   return (
 
     <div className='z-[3]'>
-    <div className="flex items-center justify-center h-full">
-      <div className="shadow-2xl  rounded-2xl border-2 border-gray-50 bg-white p-2">
+    <div className="flex items-center justify-center h-[90%]">
+      {/* <div className="shadow-2xl  rounded-2xl border-2 border-gray-50 bg-white p-2"> */}
         <div className="flex flex-col">
-    <Card className={`w-[200px] h-[200px] tablet:w-[240px] tablet:h-[240px] border-none `} style={{ background: `linear-gradient(to bottom right, #${fromColor}, #${toColor})` }}>
+    <Card className={`w-[200px] h-[200px] tablet:w-[240px] tablet:h-[220px] border-none `} style={{ background: `linear-gradient(to bottom right, #${fromColor}, #${toColor})` }}>
     <CardBody className="justify-center items-center pb-0">
       <CircularProgress
         classNames={window.innerWidth > 768 ? {
@@ -16,12 +19,17 @@ export default function MyMeter({title,reading,fromColor,toColor}) {
           indicator: "stroke-white",
           track: "stroke-white/10",
           value: "text-3xl font-semibold text-white",
-        } : {
+        } :
+         {
+          
           svg: "w-28 h-28 drop-shadow-md",
           indicator: "stroke-white",
           track: "stroke-white/10",
           value: "text-2xl font-semibold text-white",
-        }}
+        }
+      
+      }
+        // formatOptions={{style:"unit"}}
         value={reading}
         strokeWidth={4}
         showValueLabel={true}
@@ -44,7 +52,7 @@ export default function MyMeter({title,reading,fromColor,toColor}) {
             <a href="#" className="text-indigo-600 text-xs font-medium">View more</a>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   </div>
 
--- src/components/Monitor.jsx
+++ src/components/Monitor.jsx
@@ -34,7 +34,6 @@ return (
     
     {/* h-10% */}
             <div className='h-[5%] w-[90%]  flex justify-center items-center  relative'>
-  <div className='LM425:hidden absolute bottom-[3.2rem]'><MyChip/></div>
   {/* {((FirebaseContext.isTabSelected == "readings") || (FirebaseContext.isTabSelected == "gauges")) &&                */}
                 <Tabs/> 
             </div>
@@ -42,20 +41,27 @@ return (
             {
               FirebaseContext.isTabSelected == "readings"  ? (
                 <>  
-              <div className='hidden w-full h-[90%] LM425:flex  justify-center items-center gap-[8vw] mt-5 relative'>
+              <div className='hidden w-full h-[90%] LM425:flex  justify-center items-center gap-[8vw] relative'>
     
-                {((FirebaseContext.isTabSelected == "readings") || (FirebaseContext.isTabSelected == "gauges")) && (<div className='hidden LM425:block absolute bottom-14 top-2'><MyChip/></div>)}
-              
+      
                     <ReadingsCard source="https://cdn3d.iconscout.com/3d/premium/thumb/thermometer-4869739-4051728.png?f=webp" title={"Temperature"} readings={FirebaseContext.dataPacket?.temperature ?? 36.89}/>
                 
                     <ReadingsCard source="https://cdn3d.iconscout.com/3d/premium/thumb/humidity-8165662-6551904.png?f=webp" title={"Humidity"} readings={FirebaseContext.dataPacket?.humidity ?? 45.71} />
                         
+<div className='absolute -bottom-[40px]'><MyChip/></div>
+
+                    {/* {((FirebaseContext.isTabSelected == "readings") || (FirebaseContext.isTabSelected == "gauges")) && (<div className='hidden LM425:block absolute bottom-14 top-2'><MyChip/></div>)} */}
+              
               </div>  
 
+
 {/* h-90 */}
-<div className='flex w-full h-[85%] LM425:hidden  justify-center items-center '>
+<div className='flex w-full h-[85%] LM425:hidden justify-center items-center flex-col gap-6'>
     
 <MyMiniCard source="https://cdn3d.iconscout.com/3d/premium/thumb/humidity-8165662-6551904.png?f=webp" title={"Humidity & Temperature"} readings={FirebaseContext?.dataPacket ?? {} } />
+  
+  {/*  absolute bottom-[0.5rem] */}
+  <div className='LM425:hidden -pt-2'><MyChip/></div>
     
     </div> 
 </>
@@ -63,26 +69,30 @@ return (
                 
                 FirebaseContext.isTabSelected == "line-graph"  ? (
                   
-                  <div className='flex flex-col w-[100%] laptop:w-[90%] h-[75%] bg-white justify-center items-center gap-[8vw] mt-5 shadow-2xl p-2 rounded-2xl border-2 border-gray-50 relative overflow-y-hidden'>
+                                                                     // h-75
+                  <div className='flex flex-col w-[100%] laptop:w-[90%] h-[65%] laptop:h-[70%] bg-white justify-center items-center gap-[8vw] mt-14 laptop:mt-10 shadow-2xl p-2 rounded-2xl border-2 border-gray-50 relative '>
+                    {/* overflow-y-hidden */}
 
               <Text family='Jost' className='flex justify-between absolute top-0 text-cyan-400 text-xl font-semibold'><LineDropDown/></Text>
 
               {
                 FirebaseContext.slectedLineChart === "Humidity" ? (
-
+                  
                   <LineChart chartsData={HumiditylineChartsData(FirebaseContext.dataRecords) ?? [...Array(48)]}/> 
-
-                ) :  FirebaseContext.slectedLineChart === "Temperature" ?  (
-                  <LineChart chartsData={TemperaturelineChartsData(FirebaseContext.dataRecords) ?? [...Array(48)]}/>  
-                ) : <LineChart chartsData={BothlineChartsData(FirebaseContext.dataRecords) ?? [...Array(48)]}/>
-              }
+                  
+                  ) :  FirebaseContext.slectedLineChart === "Temperature" ?  (
+                    <LineChart chartsData={TemperaturelineChartsData(FirebaseContext.dataRecords) ?? [...Array(48)]}/>  
+                    ) : <LineChart chartsData={BothlineChartsData(FirebaseContext.dataRecords) ?? [...Array(48)]}/>
+                    }
                
+<div className='absolute -bottom-[40px]'><MyChip/></div>
                 </div>          
 
                         
               ) :  FirebaseContext.isTabSelected == "bar-graph"  ? (
-                  
-                <div className='flex flex-col w-[100%] laptop:w-[90%] h-[75%] bg-white justify-center items-center gap-[8vw] mt-5 shadow-2xl p-2 rounded-2xl border-2 border-gray-50 relative overflow-y-hidden'>
+                                                                      // h-75
+                <div className='flex flex-col w-[100%] laptop:w-[90%] h-[65%] laptop:h-[70%] bg-white justify-center items-center gap-[8vw] mt-14 laptop:mt-10   shadow-2xl p-2 rounded-2xl border-2 border-gray-50 relative '>
+                  {/* overflow-y-hidden */}
 
             <Text family='Jost' className='flex justify-between absolute top-0 text-cyan-400 text-xl font-semibold'><BarDropDown/></Text>
 
@@ -96,6 +106,9 @@ return (
                 <BarGraph chartsData={TemperatureBarChartsData(FirebaseContext.dataRecords) ?? [...Array(48)]}/>  
               ) : <BarGraph chartsData={BothBarChartsData(FirebaseContext.dataRecords) ?? [...Array(48)]}/>
             }
+
+
+<div className='absolute -bottom-[40px]'><MyChip/></div>
              
               </div>          
 
@@ -109,9 +122,9 @@ return (
                     <GaugesDropDown/>
 
 
+                      <div className='flex  w-full h-[90%] justify-center  items-center gap-[8vw] relative '>
 
-                      <div className='flex  w-full h-[90%] justify-center  items-center gap-[8vw] '>
-
+                      {/* {((FirebaseContext.isTabSelected == "readings") || (FirebaseContext.isTabSelected == "gauges")) && (<div className='hidden LM425:block absolute bottom-14 top-2'><MyChip/></div>)} */}
 
  {/* <div className='flex w-full h-[90%] LM425:hidden  justify-center items-center gap-[8vw] mt-5'>
     
@@ -135,15 +148,20 @@ return (
     <MyMeter  title={"Temperature"} reading={FirebaseContext.dataPacket?.temperature ?? 36.89} fromColor={"FF0000"} toColor={"ff6b6b"}/>
   ) : "" 
 }
+
+<div className='absolute -bottom-[40px]'><MyChip/></div>
+
                            
    </div>
                                 
 </div>  
         
-        <div className='flex w-full h-[78%] LM425:hidden  justify-center items-center gap-[8vw] mt-5'>
+        <div className='flex w-full h-[78%] LM425:hidden  justify-center items-center flex-col gap-6  mt-8'>
             
         <MyGaugesMini title={"Humidity & Temperature"}  readings={FirebaseContext.dataPacket ?? {}} />
             
+
+        <div className='LM425:hidden -pt-2'><MyChip/></div>
             </div> 
         </>
               ): ""        
